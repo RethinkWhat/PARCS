3b6da8076a01d3ef9fda4df1facc53b744d7fab9
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-01-27 18:18:28 +0800

    Added methods to create different types of JLabels and added an attribute of the PARCS Logo

--- src/view/Resources.java
+++ src/view/Resources.java
@@ -38,4 +38,61 @@ public class Resources {
 
     public final int resTitleLocY = 25;
 
+    public ImageIcon logoParcs = new ImageIcon("res/drawable/parcs-logo.png");
+
+    /**
+     * Creates a new JLabel with a specified text and color.
+     * The JLabel is a heading (h1).
+     * @param text The specified text.
+     * @param color The specified color.
+     * @return JLabel with specified text and color in an H1 format.
+     */
+    public JLabel createLblH1(String text, Color color) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.BOLD, 26));
+        label.setForeground(color);
+        return label;
+    }
+
+    /**
+     * Creates a new JLabel with a specified text and color.
+     * The JLabel is a heading (h2).
+     * @param text The specified text.
+     * @param color The specified color.
+     * @return JLabel with specified text and color in an H2 format.
+     */
+    public JLabel createLblH2(String text, Color color) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.BOLD, 22));
+        label.setForeground(color);
+        return label;
+    }
+
+    /**
+     * Creates a new JLabel with a specified text and color.
+     * The JLabel is a heading (h3).
+     * @param text The specified text.
+     * @param color The specified color.
+     * @return JLabel with specified text and color in an H3 format.
+     */
+    public JLabel createLblH3(String text, Color color) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.BOLD, 18));
+        label.setForeground(color);
+        return label;
+    }
+
+    /**
+     * Creates a new JLabel with a specified text and color.
+     * The JLabel is a paragraph.
+     * @param text The specified text.
+     * @param color The specified color.
+     * @return JLabel with specified text and color in a paragraph format.
+     */
+    public JLabel createLblP(String text, Color color) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.PLAIN, 16));
+        label.setForeground(color);
+        return label;
+    }
 }
