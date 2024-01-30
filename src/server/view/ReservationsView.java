64234283e94b88cba3a84d650e798a7dba043b14
RAMON EMMIEL JASMIN <ramonjasmin8@gmail.com>
2024-01-30 20:56:20 +0800

    Added wireframe for sidebar panel

--- src/server/view/ReservationsView.java
+++ src/server/view/ReservationsView.java
@@ -1,4 +1,30 @@
 package server.view;
 
-public class ReservationsView {
+import utilities.Resources;
+
+import javax.swing.*;
+
+public class ReservationsView extends JFrame {
+
+    Resources res = new Resources();
+
+    public ReservationsView(){
+        super("Dashboard");
+
+        this.setLocationRelativeTo(null);
+        this.setSize(950,560);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    class SideBarPanel extends JPanel{
+        public SideBarPanel(){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReservationsView();
+    }
 }
