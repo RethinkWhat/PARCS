0107c723917d21782b631235cb0b064d0ccc3834
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-02-01 12:50:42 +0800

    Fixed coding and file structure for the ServerStatusView

--- src/server/view/ServerStatusView.java
+++ src/server/view/ServerStatusView.java
@@ -1,4 +1,152 @@
 package server.view;
 
-public class ServerStatusView {
+import server.model.Server;
+import utilities.Resources;
+
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+
+public class ServerStatusView extends JFrame {
+
+
+    private JButton btnNavMenu;
+    private JButton btnNavHome;
+    private JButton btnNavAccount;
+    private JButton btnNavLogout;
+    private JButton btnAvailCar;
+    private JButton btnAvailMotor;
+    private JButton btnTotalBookings;
+
+
+    private JTextField txtSearchBar;
+
+
+    private JLabel lblName;
+    private JLabel lblDate;
+
+
+    Resources res = new Resources();
+
+
+    public ServerStatusView(){
+        super("Dashboard");
+
+
+        Container contentArea = new JPanel(new BorderLayout());
+
+
+        HeaderPanel pnlHeader = new HeaderPanel();
+        contentArea.add(pnlHeader, BorderLayout.NORTH);
+
+
+        JPanel pnlMain = new JPanel(new BorderLayout());
+        pnlMain.setPreferredSize(new Dimension(1100,700));
+        pnlMain.setBackground(res.lightGray);
+        pnlMain.setBorder(new EmptyBorder(25,25,25,25));
+        contentArea.add(pnlMain, BorderLayout.CENTER);
+
+
+        MainTopPanel pnlMainTop = new MainTopPanel();
+        pnlMain.add(pnlMainTop, BorderLayout.NORTH);
+
+
+        MainBottomPanel pnlMainBottom = new MainBottomPanel();
+        pnlMain.add(pnlMainBottom, BorderLayout.SOUTH);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        this.setContentPane(contentArea);
+        this.setLocationRelativeTo(null);
+        this.setSize(1300,800);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+
+
+
+
+
+    class HeaderPanel extends JPanel{
+        public HeaderPanel(){
+            setBackground(res.celadon);
+            setLayout(new FlowLayout(FlowLayout.LEFT));
+
+
+            btnNavMenu = res.createBtnIconOnly(res.logoParcs, 20, 20);
+            add(btnNavMenu);
+
+
+            this.setPreferredSize(new Dimension(1300, 40));
+
+
+        }
+    }
+
+
+    class MainTopPanel extends JPanel{
+        public MainTopPanel(){
+            setBackground(res.lightGray);
+            setLayout(new BorderLayout());
+
+
+            String userFirstName = "Ramon Emmiel";
+            lblName = new JLabel("Hello, " + userFirstName);
+            add(lblName);
+
+
+            JPanel pnlButtons = new JPanel(new GridLayout(0,3));
+            pnlButtons.setBackground(res.lightGray);
+            add(pnlButtons, BorderLayout.SOUTH);
+
+
+            JPanel pnlAvailCar = new JPanel();
+            pnlAvailCar.setBackground(res.white);
+            pnlButtons.add(pnlAvailCar);
+
+
+            this.setPreferredSize(new Dimension(1300,300));
+        }
+    }
+
+
+    class MainBottomPanel extends JPanel{
+        public MainBottomPanel(){
+            setBackground(res.white);
+            setForeground(res.white);
+            setBorder(new Resources.RoundedBorder(20));
+
+
+            setPreferredSize(new Dimension(1300,500));
+        }
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new ReservationsView();
+    }
 }
+
