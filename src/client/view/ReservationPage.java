a97a1b6af84e0b47b470edb3b33315f472417cc4
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-01 13:47:50 +0800

    Populated the main top panel. Layouting needs scaling.

--- src/client/view/ReservationPage.java
+++ src/client/view/ReservationPage.java
@@ -59,6 +59,10 @@ public class ReservationPage extends JFrame {
      * The stylesheet.
      */
     private Resources res = new Resources();
+    /**
+     * Instance variable of GridBagConstraints used for JPanels using GridBagLayout.
+     */
+    private GridBagConstraints gbc;
 
     public ReservationPage() {
         super("PARCS");
@@ -113,15 +117,19 @@ public class ReservationPage extends JFrame {
             setBackground(res.feldgrau);
 
             btnNavHome = res.createBtnIconOnly(res.iconHome,30,30);
+            btnNavHome.setHorizontalAlignment(SwingConstants.LEFT);
             add(btnNavHome);
 
             btnNavTicket = res.createBtnIconOnly(res.iconTicket, 30,30);
+            btnNavTicket.setHorizontalAlignment(SwingConstants.LEFT);
             add(btnNavTicket);
 
             btnNavAccount = res.createBtnIconOnly(res.iconAccount,30,30);
+            btnNavAccount.setHorizontalAlignment(SwingConstants.LEFT);
             add(btnNavAccount);
 
             btnNavLogout = res.createBtnIconOnly(res.iconLogout,30,30);
+            btnNavLogout.setHorizontalAlignment(SwingConstants.LEFT);
             add(btnNavLogout);
 
             this.setPreferredSize(new Dimension(60, 800));
@@ -133,19 +141,67 @@ public class ReservationPage extends JFrame {
             setBackground(res.lightGray);
             setLayout(new BorderLayout());
 
+            JPanel pnlInformation = new JPanel(new GridBagLayout());
+            pnlInformation.setPreferredSize(new Dimension(1300,50));
+            add(pnlInformation, BorderLayout.NORTH);
+            pnlInformation.setBackground(res.lightGray);
+
+            gbc = new GridBagConstraints();
+
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridwidth = 1;
+            gbc.ipadx = 475;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.anchor = GridBagConstraints.WEST;
             String userFirstName = "Ramon Emmiel";
-            lblName = new JLabel("Hello, " + userFirstName);
-            add(lblName);
+            lblName = res.createLblH2("Hello, " + userFirstName + "!", res.eerieBlack);
+            pnlInformation.add(lblName, gbc);
+
+            gbc.gridy = 1;
+            gbc.gridwidth = 3;
+            String date = "January 28, 2023 | Wednesday 10:28 AM";
+            lblDate = res.createLblH4(date, res.eerieBlack);
+            pnlInformation.add(lblDate, gbc);
+
+            gbc.gridy = 0;
+            gbc.gridx = 1;
+            gbc.gridwidth = 5;
+            gbc.anchor = GridBagConstraints.EAST;
+            gbc.fill = GridBagConstraints.BOTH;
+            txtSearchbar = res.createTxtRounded("Search date",res.white,res.gray, 30);
+            pnlInformation.add(txtSearchbar, gbc);
 
             JPanel pnlButtons = new JPanel(new GridLayout(0,3));
+            pnlButtons.setPreferredSize(new Dimension(1300,100));
+            pnlButtons.setBorder(new EmptyBorder(5,0,5,0));
             pnlButtons.setBackground(res.lightGray);
             add(pnlButtons, BorderLayout.SOUTH);
 
-            JPanel pnlAvailCar = new JPanel();
-            pnlAvailCar.setBackground(res.white);
+
+            // Update parameters as models from controller when backend has commenced.
+            ButtonPanel pnlAvailCar = new ButtonPanel(
+                    btnAvailCar = res.createBtnIconOnly(res.iconSolidCar, 50, 50),
+                    res.createLblH1("13", res.eerieBlack),
+                    res.createLblP("Available Car Slots", res.eerieBlack)
+            );
             pnlButtons.add(pnlAvailCar);
 
-            this.setPreferredSize(new Dimension(1300,300));
+            ButtonPanel pnlAvailMotor = new ButtonPanel(
+                    btnAvailMotor = res.createBtnIconOnly(res.iconSolidMotor, 50, 50),
+                    res.createLblH1("10", res.eerieBlack),
+                    res.createLblP("Available Motor Slots", res.eerieBlack)
+            );
+            pnlButtons.add(pnlAvailMotor);
+
+            ButtonPanel pnlTotalBookings = new ButtonPanel(
+                    btnTotalBookings = res.createBtnIconOnly(res.iconSolidTicket, 50,50),
+                    res.createLblH1("3", res.eerieBlack),
+                    res.createLblP("Your Total Bookings", res.eerieBlack)
+            );
+            add(pnlTotalBookings);
+
+            this.setPreferredSize(new Dimension(1300,150));
         }
     }
 
@@ -155,7 +211,32 @@ public class ReservationPage extends JFrame {
             setForeground(res.white);
             setBorder(new Resources.RoundedBorder(20));
 
-            setPreferredSize(new Dimension(1300,500));
+            setPreferredSize(new Dimension(1300,650));
+        }
+    }
+
+    class ButtonPanel extends JPanel {
+        /**
+         * Constructs a panel with a specified button, number label, and title label.
+         */
+        public ButtonPanel(JButton button, JLabel number, JLabel title) {
+            setBackground(res.white);
+            setLayout(new GridLayout());
+
+            gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5,5,5,5);
+
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            add(button, gbc);
+
+            gbc.gridx = 1;
+            add(number, gbc);
+
+            gbc.gridx = 2;
+            add(title, gbc);
+
+            this.setPreferredSize(new Dimension(200, 100));
         }
     }
 
