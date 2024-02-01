1e0941147b0ab339a87710da8cee0aafd1ff84cd
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2024-02-01 12:41:42 +0800

    Added the content area and header panel

--- src/client/view/ParkingStatus.java
+++ src/client/view/ParkingStatus.java
@@ -16,3 +16,38 @@ public class ParkingStatus extends JFrame {
 
     private Resources res = new Resources();
 
+    public ParkingStatus() {
+        super("PARCS");
+
+        // Body panel acting as a container to hold all UI components
+        Container contentArea = new JPanel(new BorderLayout());
+
+        ParkingStatus.HeaderPanel pnlHeader = new ParkingStatus.HeaderPanel();
+        contentArea.add(pnlHeader, BorderLayout.NORTH);
+
+
+        JPanel pnlMain = new JPanel(new BorderLayout());
+        pnlMain.setPreferredSize(new Dimension(1100, 700));
+        pnlMain.setBackground(res.lightGray);
+        pnlMain.setBorder(new EmptyBorder(25, 25, 25, 25));
+        contentArea.add(pnlMain, BorderLayout.CENTER);
+
+
+        this.setContentPane(contentArea);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setSize(1300, 800);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    class HeaderPanel extends JPanel {
+        public HeaderPanel() {
+            setBackground(res.celadon);
+
+            this.setPreferredSize(new Dimension(1300, 40));
+        }
+    }
+
+}
