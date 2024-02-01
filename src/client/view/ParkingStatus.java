ff05ddd110d5b1daa689d007e3d2bb977f71443a
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2024-02-01 12:17:23 +0800

    Added the variables for the ParkingStatus class

--- src/client/view/ParkingStatus.java
+++ src/client/view/ParkingStatus.java
@@ -1,4 +1,18 @@
 package client.view;
 
-public class ParkingStatus {
-}
+import utilities.Resources;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class ParkingStatus extends JFrame {
+    private JButton btnNavHome;
+    private JButton btnNavTicket;
+    private JButton btnNavAccount;
+    private JButton btnNavLogout;
+    private JButton btnbtnParkingSlotOne;
+    private JButton btnReserveSlot;
+
+    private Resources res = new Resources();
+
