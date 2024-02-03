c9c4af3c59b1920fdfead7c38a7215e2c888c992
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-03 13:23:27 +0800

    Added draft for the login model.

---
+++ src/client/model/LoginRegisterModel.java
@@ -0,0 +1,37 @@
+package client.model;
+
+import utilities.User;
+
+/**
+ * Template for LoginRegisterModel.
+ * TODO: Documentation (describe this class)
+ */
+public class LoginRegisterModel {
+    /**
+     * The status of the user.
+     * True if logged in, false if otherwise.
+     */
+    private boolean status;
+    /**
+     * The user of the program -- new or existing.
+     */
+    private User user;
+
+    /**
+     * Constructs a LoginRegisterModel with null values.
+     */
+    public LoginRegisterModel() {
+        status = false;
+        user = null;
+    }
+
+    protected void createAccount() {
+        // creates a new user
+        // XML processing here
+    }
+
+    protected void validateAccount() {
+        // checks if the user exists
+        // updates status accordingly
+    }
+}
