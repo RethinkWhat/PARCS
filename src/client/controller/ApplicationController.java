b94c1143913d8e9b249fe7573f1dfcdc0b250ab7
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-05 14:32:24 +0800

    Added template for ApplicationController.

--- src/client/controller/ApplicationController.java
+++ src/client/controller/ApplicationController.java
@@ -1,4 +1,28 @@
 package client.controller;
 
+import client.model.ApplicationModel;
+import client.view.ApplicationView;
+
+/**
+ * Template for ApplicationController object.
+ */
 public class ApplicationController {
+    /**
+     * The view ApplicationView object.
+     */
+    private ApplicationView view;
+    /**
+     * The model ApplicationModel model.
+     */
+    private ApplicationModel model;
+
+    /**
+     * Constructs an ApplicationController with a specified view and model.
+     * @param view The specified ApplicationView.
+     * @param model The specified ApplicationModel.
+     */
+    public ApplicationController(ApplicationView view, ApplicationModel model) {
+        this.view = view;
+        this.model = model;
+    }
 }
