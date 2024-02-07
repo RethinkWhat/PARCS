fd756a7bb77cde43359a48d9fa73357316d37fae
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-07 15:49:59 +0800

    Added verify password method, validateAccount method, and template methods for encrypting and decrypting passwords.

--- src/client/model/LoginRegisterModel.java
+++ src/client/model/LoginRegisterModel.java
@@ -1,7 +1,5 @@
 package client.model;
 
-import server.model.User;
-
 /**
  * Template for LoginRegisterModel.
  * TODO: Documentation (describe this class)
@@ -11,23 +9,49 @@ public class LoginRegisterModel {
      * The status of the user.
      * True if logged in, false if otherwise.
      */
-    private boolean status;
-
-    private String username;
+    private Client client;
 
     /**
      * Constructs a LoginRegisterModel with null values.
      */
-    public LoginRegisterModel() {
-        status = false;
+    public LoginRegisterModel(Client client) {
+        this.client = client;
     }
 
-    protected void createAccount() {
+    /**
+     *
+     */
+    public void createAccount(String lastName, String firstName) {
         // creates a new user
         // XML processing here
     }
 
-    protected void validateAccount() {
-        // sends login info to server
+    public boolean verifySignupPassword(String password, String passwordConfirmation) {
+        return password == passwordConfirmation;
+    }
+
+    public static String encryptPassword(String rawPassword) {
+        String encrypted = "";
+        return encrypted;
+    }
+
+    public static String decryptPassword(String encryptedPassword) {
+        String decrypted = "";
+        return decrypted;
+    }
+
+    /**
+     * Verifies the given user credentials by sending the username and password to the server.
+     * Returns true if the given credentials are mapped to an existing account. Returns false if the account
+     * does not exist or the given credentials are incorrect.
+     * @param username The specified username.
+     * @param password The specified password.
+     * @return The success state.
+     */
+    public boolean validateAccount(String username, String password) {
+        client.writeString(username);
+        client.writeString(password);
+        return client.readString().equals("true");
+        // TODO: if account exists but wrong credentials, if account does not exist.
     }
 }
