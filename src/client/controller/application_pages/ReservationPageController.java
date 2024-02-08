5999397f64dd3f18c0eb90693a79a1d2610fb3f9
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-08 16:16:33 +0800

    Added template for reservation page controller

---
+++ src/client/controller/application_pages/ReservationPageController.java
@@ -0,0 +1,19 @@
+package client.controller.application_pages;
+
+import client.model.application_pages.ReservationPageModel;
+import client.view.application_pages.ReservationPageView;
+
+public class ReservationPageController {
+    private ReservationPageView view;
+    private ReservationPageModel model;
+    public ReservationPageController(ReservationPageView view, ReservationPageModel model) {
+        this.view = view;
+        this.model = model;
+
+        // action listeners
+
+        // mouse listeners
+
+        // focus listeners
+    }
+}
