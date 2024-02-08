2aaee5a28328a73bf06390b53fa1b2be04293fc8
Johan Rickardo Roxas <2233375@slu.edu.ph>
2024-02-08 08:57:24 +0800

    Added template for application model

--- src/client/model/ApplicationModel.java
+++ src/client/model/ApplicationModel.java
@@ -1,4 +1,16 @@
 package client.model;
 
 public class ApplicationModel {
+    /**
+     * The first name of the user displayed in the home page.
+     */
+    private String userFirstName;
+
+    /**
+     * Constructs an ApplicationModel with a specified first name of the user.
+     * @param userFirstName The specified first name of the user.
+     */
+    public ApplicationModel(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
 }
