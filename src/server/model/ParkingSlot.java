072d457d17b6393f9aa8271f755b500168fa82dc
Rithik Tank <2233293@slu.edu.ph>
2024-02-11 12:39:46 +0800

    made parking spots data structure

--- src/server/model/ParkingSlot.java
+++ src/server/model/ParkingSlot.java
@@ -1,5 +1,6 @@
 package server.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingSlot {
@@ -11,4 +12,46 @@ public class ParkingSlot {
 
     // A list to hold the different dates of reservations
     private List<Reservations> reservationsList;
+
+    public ParkingSlot(String identifier, List<Reservations> reservationsList) {
+        this.identifier = identifier;
+        this.reservationsList = reservationsList;
+    }
+
+    public ParkingSlot(String identifier) {
+        this.identifier = identifier;
+        this.reservationsList = new ArrayList<>();
+    }
+
+    /**
+     * Method to get the identifier of a parking spot
+     * @return String
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /**
+     * Method to set the identifier of a parking spot
+     * @param identifier
+     */
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    /**
+     * Method to get the reservationsList
+     * @return List<Reservations
+     */
+    public List<Reservations> getReservationsList() {
+        return reservationsList;
+    }
+
+    /**
+     * Method to set the reservations List
+     * @param reservationsList : reservationsList
+     */
+    public void setReservationsList(List<Reservations> reservationsList) {
+        this.reservationsList = reservationsList;
+    }
 }
