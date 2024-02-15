c950d7541530182d8d3b8420f4cc0d6121e6896d
Johan Ricakrdo Roxas <2233375@slu.edu.ph>
2024-02-16 06:22:51 +0800

    Added method to edit vehicle info.

--- src/client/model/application_pages/UserProfileModel.java
+++ src/client/model/application_pages/UserProfileModel.java
@@ -2,6 +2,9 @@ package client.model.application_pages;
 
 import client.model.Client;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * The UserProfileModel contains the data.
  */
@@ -30,6 +33,18 @@ public class UserProfileModel {
      * The contact number of the user.
      */
     private String contactNo;
+    /**
+     * The cars of the user.
+     */
+    private String[] cars;
+    /**
+     * The motors of the user.
+     */
+    private String[] motors;
+    /**
+     * The mapping of the user to their vehicles.
+     */
+    private HashMap<String, List<String>> vehicles;
 
     /**
      * Constructs a model of UserProfile with a specified client.
@@ -57,26 +72,53 @@ public class UserProfileModel {
         password = tokens[4];
     }
 
+    /**
+     * Retrieves the current first name of the user.
+     * @return The current first name.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Retrieves the current last name of the user.
+     * @return The current last name.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Retrieves the current username of the user.
+     * @return The current username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Retrieves the current password of the user.
+     * @return The current password (encrypted).
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Retrieves the current contact number of the user.
+     * @return The current contact number.
+     */
     public String getContactNo() {
         return contactNo;
     }
 
+    /**
+     * TODO: Documentation
+     * @param firstName
+     * @param lastName
+     * @param contactNo
+     * @return
+     */
     public boolean editUserInformation(String firstName, String lastName, String contactNo) {
         client.openSocket();
         client.writeString("editInfo");
@@ -98,7 +140,35 @@ public class UserProfileModel {
         return editConfirmed;
     }
 
-    // TODO: Create method to edit password
+    /**
+     * Edits the user's vehicle information with a specified vehicle type, model, and plateNumber.
+     * @param type The specified type.
+     * @param model The specified model.
+     * @param plateNumber The specified plate number.
+     * @return True if the edit was successful. False if otherwise.
+     */
+    public boolean editVehicleInfo(String type, String model, String plateNumber) {
+        client.openSocket();;
+        client.writeString("editInfo");
+
+        // TODO: Handle equal values
+        client.writeString(username + ",vehicle," +
+                type + "," + model + "," + plateNumber);
+
+        client.writeString("complete");
+
+        boolean editConfirmed = client.readString().equals("true");
+        client.closeSocket();
+
+        return editConfirmed;
+    }
+
+    /**
+     * Changes the password of the user with a specified new password.
+     * The client sends the information to the server and parses it.
+     * @param password The specified new password.
+     * @return True if successful. False if otherwise.
+     */
     public boolean editPassword(String password) {
         client.openSocket();
         client.writeString("editPassword");
@@ -110,6 +180,11 @@ public class UserProfileModel {
         return true;
     }
 
+
+    /**
+     * Retrieves the current client.
+     * @return The current client.
+     */
     public Client getClient() {
         return client;
     }
