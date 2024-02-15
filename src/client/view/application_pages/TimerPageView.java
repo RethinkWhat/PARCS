9a7d916d98364d740014bf84412864c32c493344
Johan Ricakrdo Roxas <2233375@slu.edu.ph>
2024-02-15 21:17:48 +0800

    Fixed scaling of panels, added getters, and refactored class name of TimerPageView.

--- src/client/view/application_pages/TimerPageView.java
+++ src/client/view/application_pages/TimerPageView.java
@@ -4,14 +4,15 @@ import utilities.Resources;
 
 import javax.swing.*;
 import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * The
+ * The TimerPageView displays the current booking of the user.
  */
-public class TimerViewPage extends JPanel {
+public class TimerPageView extends JPanel {
     /**
      * The panel that holds different components.
      */
@@ -32,23 +33,26 @@ public class TimerViewPage extends JPanel {
     /**
      * Constructs a panel of TimerViewPatch.
      */
-    public TimerViewPage() {
+    public TimerPageView() {
         setLayout(new BorderLayout());
+        setBorder(new EmptyBorder(20,20,20,20));
         setBackground(res.lightGray);
+
+        container = res.createPnlRounded(1100,700,res.white, res.white);
+        container.setPreferredSize(new Dimension(1100,700));
+        container.setBorder(new EmptyBorder(20,20,20,20));
+        container.setLayout(new BorderLayout());
+        add(container, BorderLayout.CENTER);
 
         JLabel lblTitle = res.createLblH1("Current Ticket", res.eerieBlack);
         lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-        add(lblTitle, BorderLayout.CENTER);
-
-        container = res.createPnlRounded(1100,700,res.white, res.lightGray);
-        container.setLayout(new GridLayout(0,2));
-        add(container, BorderLayout.CENTER);
+        container.add(lblTitle, BorderLayout.NORTH);
 
         pnlTimer = new TimerPanel();
-        container.add(pnlTimer);
+        container.add(pnlTimer, BorderLayout.WEST);
 
         pnlTicketInfo = new TicketInfoPanel();
-        container.add(pnlTicketInfo);
+        container.add(pnlTicketInfo, BorderLayout.EAST);
 
         this.setPreferredSize(new Dimension(1100,700));
     }
@@ -102,6 +106,7 @@ public class TimerViewPage extends JPanel {
          * Constructs a panel of TimerPanel.
          */
         public TimerPanel() {
+            setBackground(res.white);
             arcExtent = 360;
             hour = 1;
             minute = 0;
@@ -118,7 +123,7 @@ public class TimerViewPage extends JPanel {
             });
             swingTimer.start();
 
-            this.setPreferredSize(new Dimension(1100,700));
+            this.setPreferredSize(new Dimension(550,650));
         }
 
         /**
@@ -175,29 +180,35 @@ public class TimerViewPage extends JPanel {
          */
         public TicketInfoPanel() {
             setLayout(new GridBagLayout());
+            setBackground(res.white);
 
             GridBagConstraints gbc = new GridBagConstraints();
             gbc.insets = new Insets(10,10,10,10);
 
             gbc.gridwidth = 1;
+            gbc.ipadx = 50;
             gbc.fill = GridBagConstraints.BOTH;
             gbc.anchor = GridBagConstraints.WEST;
 
             // left column
             gbc.gridx = 0;
             gbc.gridy = 0;
+            JLabel lblParkAreaLabel = res.createLblP("Parking Area:", res.eerieBlack);
+            add(lblParkAreaLabel, gbc);
+
+            gbc.gridy = 1;
             JLabel lblVehicleLabel = res.createLblP("Vehicle:", res.eerieBlack);
             add(lblVehicleLabel, gbc);
 
-            gbc.gridy = 1;
+            gbc.gridy = 2;
             JLabel lblParkingTypeLabel = res.createLblP("Parking Type:", res.eerieBlack);
             add(lblParkingTypeLabel, gbc);
 
-            gbc.gridy = 2;
+            gbc.gridy = 3;
             JLabel lblParkingSpotLabel = res.createLblP("Parking Spot:", res.eerieBlack);
             add(lblParkingSpotLabel, gbc);
 
-            gbc.gridy = 3;
+            gbc.gridy = 4;
             JLabel lblDateLabel = res.createLblP("Date:", res.eerieBlack);
             add(lblDateLabel, gbc);
 
@@ -205,35 +216,107 @@ public class TimerViewPage extends JPanel {
             JLabel lblDurationLabel = res.createLblP("Duration:", res.eerieBlack);
             add(lblDurationLabel, gbc);
 
-            gbc.gridy = 4;
+            gbc.gridy = 6;
             JLabel lblTimeLabel = res.createLblP("Time:", res.eerieBlack);
             add(lblTimeLabel, gbc);
 
             // right column
+            gbc.gridwidth = 1;
+
             gbc.gridx = 1;
             gbc.gridy = 0;
+            JLabel lblParkingArea = res.createLblP("SLU Maryheights Campus", res.eerieBlack);
+            add(lblParkingArea, gbc);
+
+            gbc.gridy = 1;
             lblVehicle = res.createLblP("Info", res.eerieBlack);
             add(lblVehicle, gbc);
 
-            gbc.gridy = 1;
+            gbc.gridy = 2;
             lblParkingType = res.createLblP("Info", res.eerieBlack);
             add(lblParkingType, gbc);
 
-            gbc.gridy = 2;
+            gbc.gridy = 3;
             lblParkingSpot = res.createLblP("Info", res.eerieBlack);
             add(lblParkingSpot, gbc);
 
-            gbc.gridy = 3;
+            gbc.gridy = 4;
             lblDate = res.createLblP("Info", res.eerieBlack);
             add(lblDate, gbc);
 
-            gbc.gridy = 4;
+            gbc.gridy = 5;
             lblDuration = res.createLblP("Info", res.eerieBlack);
             add(lblDuration, gbc);
 
-            gbc.gridy = 5;
+            gbc.gridy = 6;
             lblTime = res.createLblP("Info", res.eerieBlack);
             add(lblTime, gbc);
+
+            this.setPreferredSize(new Dimension(550,650));
         }
+
+        /**
+         * Retrieves the current JLabel of lblVehicle.
+         * @return The current lblVehicle.
+         */
+        public JLabel getLblVehicle() {
+            return lblVehicle;
+        }
+
+        /**
+         * Retrieves the current JLabel of lblParkingType.
+         * @return The current lblParkingType.
+         */
+        public JLabel getLblParkingType() {
+            return lblParkingType;
+        }
+
+        /**
+         * Retrieves the current JLabel of lblParkingSpot.
+         * @return The current lblParkingSpot.
+         */
+        public JLabel getLblParkingSpot() {
+            return lblParkingSpot;
+        }
+
+        /**
+         * Retrieves the current JLabel of lblDate.
+         * @return The current lblDate.
+         */
+        public JLabel getLblDate() {
+            return lblDate;
+        }
+
+        /**
+         * Retrieves the current JLabel of lblDuration.
+         * @return The current lblDuration.
+         */
+        public JLabel getLblDuration() {
+            return lblDuration;
+        }
+
+        /**
+         * Retrieves the current JLabel of lblTime.
+         * @return The current lblTime.
+         */
+        public JLabel getLblTime() {
+            return lblTime;
+        }
+    }
+
+    /**
+     * Retrieves the current JPanel of pnlTimer.
+     * @return The current pnlTimer.
+     */
+    public TimerPanel getPnlTimer() {
+        return pnlTimer;
+    }
+
+    /**
+     * Retrieves the current JPanel of pnlTicketInfo.
+     * @return The current pnlTicketInfo.
+     */
+    public TicketInfoPanel getPnlTicketInfo() {
+        return pnlTicketInfo;
     }
 }
