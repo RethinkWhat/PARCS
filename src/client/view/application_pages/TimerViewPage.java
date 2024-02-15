8b4da4e9f39c958617de2092d7956ab90fd70083
Johan Ricakrdo Roxas <2233375@slu.edu.ph>
2024-02-15 15:39:12 +0800

    Added panel classes.

--- src/client/view/application_pages/TimerViewPage.java
+++ src/client/view/application_pages/TimerViewPage.java
@@ -1,11 +1,18 @@
 package client.view.application_pages;
 
+import utilities.Resources;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
- * TODO: Documentation
+ * The
  */
 public class TimerViewPage extends JPanel {
+    /**
+     * The panel that holds different components.
+     */
+    private JPanel container;
     /**
      * The panel for the timer
      */
@@ -13,17 +20,35 @@ public class TimerViewPage extends JPanel {
     /**
      * The panel for information.
      */
-    private InfoPanel pnlInfo;
+    private TicketInfoPanel pnlTicketInfo;
+    /**
+     * The stylesheet.
+     */
+    private Resources res = new Resources();
 
     /**
      * Constructs a panel of TimerViewPatch.
      */
     public TimerViewPage() {
+        setLayout(new BorderLayout());
+        setBackground(res.lightGray);
 
+        JLabel lblTitle = res.createLblH1("Current Ticket", res.eerieBlack);
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        add(lblTitle, BorderLayout.CENTER);
+
+        container = res.createPnlRounded(1100,700,res.white, res.lightGray);
+        container.setLayout(new GridLayout(0,2));
+        add(container, BorderLayout.CENTER);
+
+        pnlTimer = new TimerPanel();
+        container.add(pnlTimer);
+
+        this.setPreferredSize(new Dimension(1100,700));
     }
 
     /**
-     *
+     * The panel to hold the timer.
      */
     public class TimerPanel extends JPanel {
         /**
@@ -64,7 +89,45 @@ public class TimerViewPage extends JPanel {
         double current;
     }
 
-    public class InfoPanel extends JPanel {
+    /**
+     * The panel to hold the ticket information of the booking.
+     */
+    public class TicketInfoPanel extends JPanel {
+        /**
+         * The label of the vehicle parked.
+         */
+        private JLabel lblVehicle;
+        /**
+         * The label of the parking type.
+         */
+        private JLabel lblParkingType;
+        /**
+         * The label of the parking spot.
+         */
+        private JLabel lblParkingSpot;
+        /**
+         * The label of booked parking.
+         */
+        private JLabel lblDate;
+        /**
+         * The label of the parking duration in hours.
+         */
+        private JLabel lblDuration;
+        /**
+         * The label for the time-in and time-out of the booking.
+         */
+        private JLabel lblHours;
 
+        /**
+         * Constructs a panel of TicketInfoPanel.
+         */
+        public TicketInfoPanel() {
+            setLayout(new GridBagLayout());
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(10,10,10,10);
+
+            JLabel lblVehicleLabel;
+        }
     }
 }
